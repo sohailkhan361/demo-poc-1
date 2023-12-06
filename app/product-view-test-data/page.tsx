982d0e8ebd5d30475fd3857b6034610cc4eb140a
Sohail Khan <sohail.khan@particle41.com>
2023-12-06 11:47:55 +0530

    Resolve hydration error, passing data ro BuilderComponent

--- app/product-view-test-data/page.tsx
+++ app/product-view-test-data/page.tsx
@@ -1,8 +1,9 @@
 // Passing data to BuilderComponent page through code
 /* eslint-disable @next/next/no-async-client-component */
 'use client';
+import { useEffect, useState } from "react";
 import { builder } from "@builder.io/sdk";
-import { BuilderComponent, useIsPreviewing } from "@builder.io/react";
+import { BuilderComponent } from "@builder.io/react";
 
 builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
 
@@ -12,26 +13,39 @@ interface PageProps {
     };
 }
 
-export default async function Page(props: PageProps) {
-        const content = await builder
-        .get("page", {
-            userAttributes: {
-                urlPath: "/product-view-test-data" + (props?.params?.page?.join("/") || ""),
-            },
-        })
-        .toPromise();
+export default function Page(props: PageProps) {
+    const [content, setContent] = useState(null);
 
-        return (
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await builder
+                .get("page", {
+                    userAttributes: {
+                        urlPath: "/product-view-test-data" + (props?.params?.page?.join("/") || ""),
+                    },
+                })
+                .toPromise();
+            setContent(result);
+        };
+
+        fetchData();
+    }, [props.params.page]);
+
+    return (
+        <div>
+            {content && (
                 <BuilderComponent
-                    content={content} 
-                    model="page" 
+                    content={content}
+                    model="page"
                     data={{
-                        "name": "JON DOE", 
-                        "company": "ABC", 
-                        "description": "Example of passing data to BuilderComponent page through code. This is my own description supplied through my code.",
-                        "list": [1,2,3,4],
-                        "obj": [{"val": 1}, {"val": 2}, {"val": 2}]
+                        name: "JON DOE",
+                        company: "ABC",
+                        description: "Example of passing data to BuilderComponent page through code. This is my own description supplied through my code.",
+                        list: [1, 2, 3, 4],
+                        obj: [{ val: 1 }, { val: 2 }, { val: 2 }],
                     }}
                 />
-        );
-};
+            )}
+        </div>
+    );
+}
